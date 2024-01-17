3adc1abee4fda802a9bcb7e5cbd30559c1126fb0
Hari Sirigouni <Sirigounih@us.fctg.travel>
2024-01-17 18:47:35 +0000

    test

---
+++ src/components/pages/FirstPage.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { AuthState } from "@okta/okta-auth-js";
+import { useAuth } from "@okta/okta-vue";
+import {   ShallowRef, inject, ref } from "vue";
+
+    const count = ref(0)
+    const authState = inject<ShallowRef<AuthState>>('okta.authState');
+
+    console.log('authState')
+
+    const $auth = useAuth();
+
+    const logout = async () => {
+      await $auth.signOut();
+    } 
+</script>
+
+<template>
+  <div class="card">
+    <p>
+      This is from the FirstPage. 
+    </p>
+    Welcome back <h5> {{ authState?.idToken?.claims.name }} </h5> Click the button below to increase the count or click to logout. <br/>
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <Button @click="logout()" type="text">Logout</Button>
+  </div> 
+</template>
+<style>
+  button{
+    margin: 0 0.5rem;
+    background-color: lightgrey;
+  }
+</style>
---
+++ src/components/router/index.ts
@@ -0,0 +1,26 @@
+import { LoginCallback, navigationGuard } from "@okta/okta-vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+    {
+        path: '/my-test-app/',
+        component: () => import("../pages/FirstPage.vue"),
+        name: 'First Page',
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/my-test-app/login/callback',
+        component: LoginCallback,
+        redirectUri: './index.html'
+    }
+]
+
+
+export const router = createRouter({
+    history: createWebHistory('/'),
+    routes,
+})
+
+router.beforeEach(navigationGuard);
